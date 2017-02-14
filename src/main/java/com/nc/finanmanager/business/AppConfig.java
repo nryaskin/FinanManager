16132d0f2eb1860fa72121c5ed244b25b1f83e32
nryaskin <nik.ryaskin@gmail.com>
2017-02-14 14:57:21 +0300

    basic dao and beans

---
+++ src/main/java/com/nc/finanmanager/business/AppConfig.java
@@ -0,0 +1,12 @@
+
+package com.nc.finanmanager.business;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("com.nc.com.nc.finanmanager.business")
+public class AppConfig {
+    
+}
